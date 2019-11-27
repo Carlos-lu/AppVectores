d515c30b52d77fcc1ded7e4aea0cbd540c2b16ac
Fernando10g97 <fernando10g97@gmail.com>
2019-11-27 12:09:40 -0500

    FORMULARIO VECTORES

--- AppVectores/Form1.Designer.cs
+++ AppVectores/Form1.Designer.cs
@@ -28,13 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtEntero = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstEnterosIngresados = new System.Windows.Forms.ListBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnMostrarRegistro = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPromedio = new System.Windows.Forms.TextBox();
+            this.txtSuma = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtEntero
+            // 
+            this.txtEntero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEntero.Location = new System.Drawing.Point(120, 33);
+            this.txtEntero.Name = "txtEntero";
+            this.txtEntero.Size = new System.Drawing.Size(50, 22);
+            this.txtEntero.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese un entero   :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Lista de enteros ingresados  : ";
+            // 
+            // lstEnterosIngresados
+            // 
+            this.lstEnterosIngresados.FormattingEnabled = true;
+            this.lstEnterosIngresados.Location = new System.Drawing.Point(15, 88);
+            this.lstEnterosIngresados.Name = "lstEnterosIngresados";
+            this.lstEnterosIngresados.Size = new System.Drawing.Size(145, 108);
+            this.lstEnterosIngresados.TabIndex = 3;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(202, 35);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(152, 23);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnMostrarRegistro
+            // 
+            this.btnMostrarRegistro.Location = new System.Drawing.Point(202, 72);
+            this.btnMostrarRegistro.Name = "btnMostrarRegistro";
+            this.btnMostrarRegistro.Size = new System.Drawing.Size(152, 23);
+            this.btnMostrarRegistro.TabIndex = 5;
+            this.btnMostrarRegistro.Text = "MOSTRAR REGISTRO";
+            this.btnMostrarRegistro.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(166, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Promedio     :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(166, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Suma           :";
+            // 
+            // txtPromedio
+            // 
+            this.txtPromedio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPromedio.Location = new System.Drawing.Point(241, 106);
+            this.txtPromedio.Name = "txtPromedio";
+            this.txtPromedio.Size = new System.Drawing.Size(59, 22);
+            this.txtPromedio.TabIndex = 8;
+            // 
+            // txtSuma
+            // 
+            this.txtSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSuma.Location = new System.Drawing.Point(241, 154);
+            this.txtSuma.Name = "txtSuma";
+            this.txtSuma.Size = new System.Drawing.Size(59, 22);
+            this.txtSuma.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(368, 209);
+            this.Controls.Add(this.txtSuma);
+            this.Controls.Add(this.txtPromedio);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnMostrarRegistro);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.lstEnterosIngresados);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEntero);
+            this.Name = "Form1";
+            this.Text = "VECTORES";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtEntero;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstEnterosIngresados;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnMostrarRegistro;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPromedio;
+        private System.Windows.Forms.TextBox txtSuma;
     }
 }
 
--- AppVectores/Form1.cs
+++ AppVectores/Form1.cs
@@ -16,5 +16,19 @@ namespace AppVectores
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            int[] numeros = new int[10];
+
+            int entero = int.Parse(this.txtEntero.Text);
+
+
+        }
     }
 }
